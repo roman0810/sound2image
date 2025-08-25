90ab9d6c493759d42c907bbbd6ffd3d884d2bb08
roman0810 <roman0810.r@gmail.com>
2025-08-25 16:25:29 +0300

    FSDP typing problem
    откатились к старой версии unet.py, FSDP меняет классы объектов которые попадают в SwitchSequential из-за чего становится возникает путаница в наборе аргументов необходимых для запуска layer.

--- models/unet.py
+++ models/unet.py
@@ -126,86 +126,51 @@ class SwitchSequential(nn.Sequential):
 class UNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.GPUs = ["cuda:0", "cuda:1", "cuda:2", "cuda:3", "cuda:4", "cuda:5", "cuda:6", "cuda:7"]
-
-        self.encoder1 = nn.ModuleList([
+        self.encoders = nn.ModuleList([
             SwitchSequential(nn.Conv2d(3, 32, kernel_size=3, padding=1)),
             SwitchSequential(ResidualBlock(32, 32), AttentionBlock(8, 4, self_att=False)),
             SwitchSequential(ResidualBlock(32, 32), AttentionBlock(8, 4, self_att=False)),
-            SwitchSequential(nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1))
-            ]).to(self.GPUs[1])
-
-        self.encoder2 = nn.ModuleList([
+            SwitchSequential(nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)),
             SwitchSequential(ResidualBlock(32, 64), AttentionBlock(8, 8, self_att=True)),
             SwitchSequential(ResidualBlock(64, 64), AttentionBlock(8, 8, self_att=True)),
-            SwitchSequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1))
-            ]).to(self.GPUs[2])
-
-        self.encoder3 = nn.ModuleList([
+            SwitchSequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)),
             SwitchSequential(ResidualBlock(64, 128), AttentionBlock(8, 16, self_att=True)),
             SwitchSequential(ResidualBlock(128, 128), AttentionBlock(8, 16, self_att=True)),
             SwitchSequential(nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1)),
             SwitchSequential(ResidualBlock(128, 128)),
             SwitchSequential(ResidualBlock(128, 128)),
-        ]).to(self.GPUs[3])
-
+        ])
         self.bottleneck = SwitchSequential(
             ResidualBlock(128, 128),
             AttentionBlock(8, 16),
             ResidualBlock(128, 128),
-        ).to(self.GPUs[4])
-
-        self.decoder1 = nn.ModuleList([
+        )
+        self.decoders = nn.ModuleList([
             SwitchSequential(ResidualBlock(256, 128)),
             SwitchSequential(ResidualBlock(256, 128)),
             SwitchSequential(ResidualBlock(256, 128), Upsample(128)),
             SwitchSequential(ResidualBlock(256, 128), AttentionBlock(8, 16, self_att=True)),
-            SwitchSequential(ResidualBlock(256, 128), AttentionBlock(8, 16, self_att=True))
-            ]).to(self.GPUs[5])
-
-        self.decoder2 = nn.ModuleList([
+            SwitchSequential(ResidualBlock(256, 128), AttentionBlock(8, 16, self_att=True)),
             SwitchSequential(ResidualBlock(192, 128), AttentionBlock(8, 16, self_att=True), Upsample(128)),
             SwitchSequential(ResidualBlock(192, 64), AttentionBlock(8, 8, self_att=True)),
-            SwitchSequential(ResidualBlock(128, 64), AttentionBlock(8, 8, self_att=True))
-            ]).to(self.GPUs[6])
-
-        self.decoder3 = nn.ModuleList([
+            SwitchSequential(ResidualBlock(128, 64), AttentionBlock(8, 8, self_att=True)),
             SwitchSequential(ResidualBlock(96, 64), AttentionBlock(8, 8, self_att=True), Upsample(64)),
             SwitchSequential(ResidualBlock(96, 32), AttentionBlock(8, 4, self_att=False)),
             SwitchSequential(ResidualBlock(64, 32)),
             SwitchSequential(ResidualBlock(64, 32)),
-        ]).to(self.GPUs[7])
+        ])
 
     def forward(self, x, context, time):
         skip_connections = []
-        # проводим операции каждого блока на своей карте и кладем результаты на карту где их будут принимать
-        for layers in self.encoder1:
-            x = layers(x.to(self.GPUs[1]), context.to(self.GPUs[1]), time.to(self.GPUs[1]))
-            skip_connections.append(x.to(self.GPUs[7]))
+        for layers in self.encoders:
+            x = layers(x, context, time)
+            skip_connections.append(x)
 
-        for layers in self.encoder2:
-            x = layers(x.to(self.GPUs[2]), context.to(self.GPUs[2]), time.to(self.GPUs[2]))
-            skip_connections.append(x.to(self.GPUs[6]))
+        x = self.bottleneck(x, context, time)
 
-        for layers in self.encoder3:
-            x = layers(x.to(self.GPUs[3]), context.to(self.GPUs[3]), time.to(self.GPUs[3]))
-            skip_connections.append(x.to(self.GPUs[5]))
-
-
-        x = self.bottleneck(x.to(self.GPUs[4]), context.to(self.GPUs[4]), time.to(self.GPUs[4]))
-
-
-        for layers in self.decoder1:
-            x = torch.cat((x.to(self.GPUs[5]), skip_connections.pop()), dim=1)
-            x = layers(x, context.to(self.GPUs[5]), time.to(self.GPUs[5]))
-
-        for layers in self.decoder2:
-            x = torch.cat((x.to(self.GPUs[6]), skip_connections.pop()), dim=1)
-            x = layers(x, context.to(self.GPUs[6]), time.to(self.GPUs[6]))
-
-        for layers in self.decoder3:
-            x = torch.cat((x.to(self.GPUs[7]), skip_connections.pop()), dim=1)
-            x = layers(x, context.to(self.GPUs[7]), time.to(self.GPUs[7]))
+        for layers in self.decoders:
+            x = torch.cat((x, skip_connections.pop()), dim=1)
+            x = layers(x, context, time)
 
         return x
 
@@ -225,16 +190,16 @@ class FinalLayer(nn.Module):
 class UNetWithCrossAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.time_embedding = TimeEmbedding(320).to("cuda:0")
+        self.time_embedding = TimeEmbedding(320)
         self.unet = UNet()
-        self.final = FinalLayer(32, 3).to("cuda:0")
+        self.final = FinalLayer(32, 3)
         self.audio_ctx_dim = config.audio_ctx_dim
 
-    def forward(self, x, time, context=None):
+    def forward(self, latent, time, context=None):
         if context is None:
-            context = torch.zeros(x.shape[0], 1, self.audio_ctx_dim).to(x.device)
+            context = torch.zeros(latent.shape[0], 1, self.audio_ctx_dim).to(latent.device)
 
-        time = self.time_embedding(time.to("cuda:0"))
-        output = self.unet(x, context, time)
-        output = self.final(output.to("cuda:0"))
+        time = self.time_embedding(time)
+        output = self.unet(latent, context, time)
+        output = self.final(output)
         return output
--- train_fsdp.py
+++ train_fsdp.py
@@ -12,47 +12,51 @@ import os
 import argparse
 
 from utils.config import ModelConfig
-from models.unet import UNetWithCrossAttention
+from models.unet import UNetWithCrossAttention, ResidualBlock, AttentionBlock
 from models.diffusion import Diffusion
 from utils.EmbedsDataset import EmbedsDataset
+import time
 
 
 class FSDP_Trainer:
-	def __init__(self,
-		model: torch.nn.Module,
-		train_data: EmbedsDataset,
-		val_data: EmbedsDataset,
-		config: ModelConfig):
+    def __init__(self,
+        model: torch.nn.Module,
+        train_data: EmbedsDataset,
+        val_data: EmbedsDataset,
+        config: ModelConfig):
 
-	    local_rank = int(os.environ["LOCAL_RANK"])
-	    self.rank = int(os.environ["RANK"])
-	    world_size = int(os.environ["WORLD_SIZE"])
+        local_rank = int(os.environ["LOCAL_RANK"])
+        self.rank = int(os.environ["RANK"])
+        world_size = int(os.environ["WORLD_SIZE"])
 
-	    init_process_group(backend="nccl")
+        # не поддерживается GPU на суперкомпе
+        # os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
-	    torch.cuda.set_device(self.rank)
+        init_process_group(backend="nccl")
 
-	    # --- data ---
-	    self.train_data = DataLoader(
-	    	train_data,
-	        batch_size=config.BS,
-	        num_workers=8,
-	        pin_memory=True,
-	        shuffle=False,
-	        sampler=DistributedSampler(train_data, shuffle=True, drop_last=True),
-	        drop_last = True
-	    )
-	    self.val_data = DataLoader(
-	    	val_data,
-	        batch_size=config.BS,
-	        num_workers=8,
-	        pin_memory=True,
-	        shuffle=False,
-	        sampler=DistributedSampler(val_data, shuffle=False, drop_last=True),
-	        drop_last = True
-	    )
+        torch.cuda.set_device(self.rank)
 
-	    # --- train tools ---
+        # --- data ---
+        self.train_data = DataLoader(
+            train_data,
+            batch_size=config.BS,
+            num_workers=8,
+            pin_memory=True,
+            shuffle=False,
+            sampler=DistributedSampler(train_data, shuffle=True, drop_last=True),
+            drop_last = True
+        )
+        self.val_data = DataLoader(
+            val_data,
+            batch_size=config.BS,
+            num_workers=8,
+            pin_memory=True,
+            shuffle=False,
+            sampler=DistributedSampler(val_data, shuffle=False, drop_last=True),
+            drop_last = True
+        )
+
+        # --- train tools ---
         self.unconditional_prob = config.unconditional_prob
         self.save_every = config.save_every
 
@@ -65,8 +69,8 @@ class FSDP_Trainer:
         self.perceptual_scale = 0.2
         self.difference = 2.5
 
-	    # --- model ---
-	    self.model = model.to(self.rank)
+        # --- model ---
+        self.model = model.to(self.rank)
 
         if os.path.exists(config.snapshot_path):
             LR = self._load_snapshot(config.snapshot_path)
@@ -74,30 +78,33 @@ class FSDP_Trainer:
             print(f'GPU[{self.rank}]: Snapshot path {config.snapshot_path} does not exist')
             LR = config.lr
 
-	    self.model = FSDP(
-	        self.model,
-	        auto_wrap_policy=fsdp_auto_wrap_policy,
-	        mixed_precision=torch.distributed.fsdp.MixedPrecision(
-	            param_dtype=torch.bfloat16,
-	            reduce_dtype=torch.bfloat16,
-	            buffer_dtype=torch.bfloat16,
-	        ),
-	        sharding_strategy=torch.distributed.fsdp.ShardingStrategy.FULL_SHARD,
-	        device_id=rank,
-	        use_orig_params=True,
-	    )
+        def fsdp_auto_wrap_policy(module, recurse, nonwrapped_numel):
+            return isinstance(module, (ResidualBlock, AttentionBlock))
+
+        self.model = FSDP(
+            self.model,
+            auto_wrap_policy=fsdp_auto_wrap_policy,
+            mixed_precision=torch.distributed.fsdp.MixedPrecision(
+                param_dtype=torch.bfloat16,
+                reduce_dtype=torch.bfloat16,
+                buffer_dtype=torch.bfloat16,
+            ),
+            sharding_strategy=torch.distributed.fsdp.ShardingStrategy.FULL_SHARD,
+            device_id=self.rank,
+            use_orig_params=True,
+        )
         self.diffusion = Diffusion(
-        	timesteps=config.timesteps, 
+            timesteps=config.timesteps,
             image_size=config.image_size,
             device=torch.device(f'cuda:{self.rank}')
         )
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr = LR)
         self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=config.gamma)
         self.scaler = GradScaler()
-	    if config.compile:
-	        self.model = torch.compile(self.model)
+        if config.compile:
+            self.model = torch.compile(self.model)
 
-	 def _load_snapshot(self, snapshot_path):
+    def _load_snapshot(self, snapshot_path):
         snapshot = torch.load(snapshot_path)
         self.model.load_state_dict(snapshot["MODEL_STATE"])
         self.epochs_run = snapshot["EPOCHS_RUN"]
@@ -115,9 +122,14 @@ class FSDP_Trainer:
         start_time = time.time()
         data_size = len(self.train_data)
 
-        train_epo_noise_losses = []
-        train_epo_feature_losses = []
-        val_epo_losses = []
+        # потери обязаны быть синхронизированы между всеми GPU, храним их в тензорах
+
+        train_epo_noise_losses = torch.tensor(0.0).to(self.rank)
+        train_epo_feature_losses = torch.tensor(0.0).to(self.rank)
+        val_epo_losses = torch.tensor(0.0).to(self.rank)
+
+        train_samples_done = torch.tensor(0).to(self.rank)
+        val_samples_done = torch.tensor(0).to(self.rank)
 
         self.model.train()
         for source, targets in self.train_data:
@@ -125,8 +137,18 @@ class FSDP_Trainer:
             targets = targets.to(self.rank)
 
             noise_loss, feature_loss = self._train_batch(source, targets)
-            train_epo_noise_losses.append(noise_loss)
-            train_epo_feature_losses.append(feature_loss)
+            train_epo_noise_losses += noise_loss
+            train_epo_feature_losses += feature_loss
+
+            train_samples_done += torch.tensor(1.0).to(self.rank)
+
+        # синхронизируем тренировочные потери на всех GPU
+        torch.distributed.all_reduce(train_epo_noise_losses, op=torch.distributed.ReduceOp.SUM)
+        torch.distributed.all_reduce(train_epo_feature_losses, op=torch.distributed.ReduceOp.SUM)
+        torch.distributed.all_reduce(train_samples_done, op=torch.distributed.ReduceOp.SUM)
+
+        self.train_noise_losses.append((train_epo_noise_losses/train_samples_done).item())
+        self.train_feature_losses.append((train_epo_feature_losses/train_samples_done).item())
 
         self.scheduler.step()
 
@@ -135,23 +157,26 @@ class FSDP_Trainer:
             source = source.to(self.rank)
             targets = targets.to(self.rank)
 
-            val_epo_losses.append(self._validate_batch(source, targets))
+            val_epo_losses += self._validate_batch(source, targets)
+            val_samples_done += torch.tensor(1.0).to(self.rank)
 
-        self.train_noise_losses.append(sum(train_epo_noise_losses)/len(train_epo_noise_losses))
-        self.train_feature_losses.append(sum(train_epo_feature_losses)/len(train_epo_feature_losses))
-        self.val_losses.append(sum(val_epo_losses)/len(val_epo_losses))
+
+        torch.distributed.all_reduce(val_epo_losses, op=torch.distributed.ReduceOp.SUM)
+        torch.distributed.all_reduce(val_samples_done, op=torch.distributed.ReduceOp.SUM)
+
+        self.val_losses.append((val_epo_losses/val_samples_done).item())
 
         print(f'GPU[{self.rank}]: Epoch {epoch} | Time {int(time.time()-start_time)}')
 
 
     def _train_batch(self, source, targets):
-    	# unconditional_prob - вероятность безусловной генерации
+        # unconditional_prob - вероятность безусловной генерации
         if torch.rand(1) < self.unconditional_prob:
             source = None
                 
         self.optimizer.zero_grad()
         with autocast('cuda', dtype=torch.bfloat16):
-        	noise_loss, feature_loss = self.diffusion.self_perceptual_loss(self.model, targets, source)
+            noise_loss, feature_loss = self.diffusion.self_perceptual_loss(self.model, targets, source)
 
         # балансировка noise и self_perceptual потерь
         if (self.perceptual_scale*self.difference*feature_loss > noise_loss).item():
@@ -163,7 +188,100 @@ class FSDP_Trainer:
         self.scaler.step(optimizer)
         self.scaler.update()
 
-        return noise_loss.item(), feature_loss.item()*self.perceptual_scale
+        return noise_loss, feature_loss*self.perceptual_scale
+
+    # ВНИМАНИЕ! Валидация считается ТОЛЬКО по мини-батчу каждой конкретной GPU
+    @torch.no_grad()
+    def _validate_batch(self, source, targets):
+        with autocast('cuda', dtype=torch.bfloat16):
+            loss = self.diffusion.loss_fn(self.model, targets, source)
+
+        return loss
+
+    def _save_snapshot(self, epoch, name):
+        # только процесс GPU:0 сохраняет модель
+        if self.rank != 0:
+            print(f"GPU[{self.rank}] Warning! The save procces was skipped")
+            return
+
+        # ждем завершения всех процессов
+        torch.distributed.barrier()
+
+        # загружаем на GPU:0 все веса
+        # ВНИМАНИЕ! Модель может не влезть на 1 карту
+        model_state_dict = FSDP.full_state_dict(self.model, rank0_only=True)
+
+        snapshot = {}
+        # ВНИМАНИЕ! Возможны конфликты префиксов из-за обертки FSDP
+        snapshot["MODEL_STATE"] = self.model.module.state_dict()
+        snapshot["EPOCHS_RUN"] = epoch
+        snapshot["TRAIN_NOISE_LOSSES"] = self.train_noise_losses
+        snapshot["TRAIN_FEATURE_LOSSES"] = self.train_feature_losses
+        snapshot["VAL_LOSSES"] = self.val_losses
+        snapshot["LR"] = self.scheduler.get_last_lr()[0]
+        snapshot["SP_SCALE"] = self.perceptual_scale
+
+        torch.save(snapshot, f"{name}.pt")
+        print(f'Epoch {epoch} | Training snapshot saved at {name}.pt')
+
+    def train(self, max_epochs: int):
+        b_sz = len(next(iter(self.train_data))[0])
+        data_size = len(self.train_data)
+
+        print(f"Training: Epoches {self.epochs_run}:{max_epochs} | BS {b_sz} | Batches {data_size}")
+        for epoch in range(self.epochs_run, max_epochs):
+            self.train_data.sampler.set_epoch(epoch)
+            self._run_epoch(epoch)
+            if self.local_rank == 0 and epoch % self.save_every == 0:
+                self._save_snapshot(epoch, "snapshot")
+
+        self._save_snapshot(epoch, "result")
+
 
     def __del__(self):
-    	destroy_process_group()
+        destroy_process_group()
+
+
+# ВНИМАНИЕ! snapshot_path - это адрес загружаемой модели. Обученная модель будет сохранена как result.pt
+# на текущей версии доля безусловной генерации фиксированна
+def main(save_every: int, total_epochs: int, snapshot_path: str = "snapshot.pt"):
+    # задаем параметры инициализации
+    config = ModelConfig({"image_size": 128,
+                          "sample_rate": 48000,
+                          "audio_ctx_dim": 768,
+                          "image_path": "data/images",
+                          "embed_path": "data/embeds/sound_embeds.h5",
+                          "lr": 0.0005,
+                          "gamma": 0.98,
+                          "BS": 3,
+                          "unconditional_prob": 0.1,
+                          "timesteps": 1000,
+                          "save_every": save_every,
+                          "snapshot_path": snapshot_path,
+                          "compile": False})
+
+    # инициализируем датасет и модель
+    dataset = EmbedsDataset(config.image_path, config.embed_path)
+    train_datset, val_dataset = torch.utils.data.random_split(dataset, [len(dataset)-8000, 8000])
+    model = UNetWithCrossAttention(config)
+
+    trainer = FSDP_Trainer(
+        model,
+        train_datset,
+        val_dataset,
+        config
+        )
+
+    trainer.train(total_epochs)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--total-epochs", type=int, help="Общее количество эпох (int)")
+    parser.add_argument("--save-every", type=int, help="Интервал сохранения модели (int)")
+
+    args = parser.parse_args()
+
+    main(args.save_every, args.total_epochs)
